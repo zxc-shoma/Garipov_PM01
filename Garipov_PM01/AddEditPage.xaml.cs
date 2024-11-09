4f4e232410a260c7043d61e4fb49dc2be1f2b627
shoma <shoma@DESKTOP-S758PBJ>
2024-11-09 17:09:27 +0500

    Добавьте файлы проекта.

---
+++ Garipov_PM01/AddEditPage.xaml.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Garipov_PM01
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Page
+    {
+
+        private DOCTORS _currentDoctor = new DOCTORS();
+        public AddEditPage(DOCTORS selectedDoctor)
+        {
+            InitializeComponent();
+            if (selectedDoctor != null)
+            {
+                _currentDoctor = selectedDoctor;
+                ComboType.SelectedIndex = _currentDoctor.DOCTOR_DEPARTAMENT_ID - 2 ;
+            }
+
+
+            DataContext = _currentDoctor;
+        }
+
+        private void UploadPhotoButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            OpenFileDialog myOpenFileDialog = new OpenFileDialog();
+            if (myOpenFileDialog.ShowDialog() == true)
+            {
+
+                FileInfo fileInfo = new FileInfo(myOpenFileDialog.FileName);
+
+
+                if (fileInfo.Length < 2 * 1024 * 1024)
+                {
+                    _currentDoctor.DOCTOR_PHOTO = myOpenFileDialog.FileName;
+                    _photo.Source = new BitmapImage(new Uri(myOpenFileDialog.FileName));
+                }
+                else
+                {
+
+                    MessageBox.Show("Размер файла превышает 2 мегабайта. Выберите другой файл.");
+                }
+            }
+
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder err = new StringBuilder();
+
+            if (string.IsNullOrWhiteSpace(_currentDoctor.DOCTOR_NAME))
+                err.AppendLine("Укажите имя врача");
+            if (string.IsNullOrWhiteSpace(_currentDoctor.DOCTOR_SURNAME))
+                err.AppendLine("Укажите фамилию врача");
+            if (string.IsNullOrWhiteSpace(_currentDoctor.DOCTOR_PATRONYMIC))
+                err.AppendLine("Укажите отчество врача");
+            if (string.IsNullOrWhiteSpace(_currentDoctor.DOCTOR_POST))
+                err.AppendLine("Укажите должность врача");
+            if (string.IsNullOrWhiteSpace(_currentDoctor.DOCTOR_SCIENTIFIC))
+                err.AppendLine("Укажите образование врача");
+            if (_currentDoctor.DOCTOR_EXPERIENCE == DateTime.MinValue)
+
+                err.AppendLine("Укажите дату выхода на работу врача");
+
+            if (string.IsNullOrWhiteSpace(_currentDoctor.DOCTOR_ADRESS))
+                err.AppendLine("Укажите адрес врача");
+            if (ComboType.SelectedItem == null)
+                err.AppendLine("Укажите отделение ");
+              else
+                {
+
+                    _currentDoctor.DOCTOR_DEPARTAMENT_ID = ComboType.SelectedIndex + 2 ;
+                }
+
+            if (err.Length > 0)
+            {
+                MessageBox.Show(err.ToString());
+                return;
+            }
+
+            if (_currentDoctor.DOCTOR_ID == 0)
+               PolyclinicEntities.GetContext().DOCTORS.Add(_currentDoctor);
+
+            try
+            {
+                PolyclinicEntities.GetContext().SaveChanges();
+                MessageBox.Show("информация сохранена");
+                manager.MainFrame.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+
+        }
+    }
+}
---
+++ Garipov_PM01/doctorPage.xaml.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Garipov_PM01
+{
+    /// <summary>
+    /// Логика взаимодействия для doctorPage.xaml
+    /// </summary>
+    public partial class doctorPage : Page
+    {
+        public doctorPage()
+        {
+            InitializeComponent();
+
+            var currentDoctor = PolyclinicEntities.GetContext().DOCTORS.ToList();
+            DoctorListView.ItemsSource = currentDoctor;
+
+
+            DepartmentComboBox.SelectedIndex = 0;
+
+            Update();
+            DoctorListView.Items.Refresh();
+        }
+
+        private void Update()
+        {
+            var currentDoctor = PolyclinicEntities.GetContext().DOCTORS.ToList();
+
+            var start_count = currentDoctor.Count;
+
+            if (DepartmentComboBox.SelectedIndex == 0)
+            {
+                currentDoctor = currentDoctor;
+            }
+            if (DepartmentComboBox.SelectedIndex == 1)
+            {
+                currentDoctor = currentDoctor.Where(p => (p.DOCTOR_DEPARTAMENT_ID == 2)).ToList();
+            }
+            if (DepartmentComboBox.SelectedIndex == 2)
+            {
+                currentDoctor = currentDoctor.Where(p => (p.DOCTOR_DEPARTAMENT_ID == 3)).ToList();
+            }
+            if (DepartmentComboBox.SelectedIndex == 3)
+            {
+                currentDoctor = currentDoctor.Where(p => (p.DOCTOR_DEPARTAMENT_ID == 4)).ToList();
+            }
+            if (DepartmentComboBox.SelectedIndex == 4)
+            {
+                currentDoctor = currentDoctor.Where(p => (p.DOCTOR_DEPARTAMENT_ID == 5)).ToList();
+            }
+            if (DepartmentComboBox.SelectedIndex == 5)
+            {
+                currentDoctor = currentDoctor.Where(p => (p.DOCTOR_DEPARTAMENT_ID == 6)).ToList();
+            }
+
+            currentDoctor = currentDoctor.Where(p => p.DOCTOR_NAME.ToLower().Contains(SearchBox.Text.ToLower()) ||
+            p.DOCTOR_SURNAME.ToLower().Contains(SearchBox.Text.ToLower()) ||
+            p.DOCTOR_PATRONYMIC.ToLower().Contains(SearchBox.Text.ToLower())).ToList();
+
+            if (SortAscRadioButton.IsChecked.Value)
+            {
+                currentDoctor = currentDoctor.OrderBy(p => p.DOCTOR_WORK).ToList();
+
+            }
+            if (SortDescRadioButton.IsChecked.Value)
+            {
+                currentDoctor = currentDoctor.OrderByDescending(p => p.DOCTOR_WORK).ToList();
+            }
+
+            TBCount.Text = string.Format("{0} из {1}", currentDoctor.Count, start_count);
+
+            DoctorListView.ItemsSource = currentDoctor;
+
+
+
+
+        }
+
+
+
+        private void DepartmentComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Update();
+            DoctorListView.Items.Refresh();
+
+        }
+
+        private void SortAscRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            Update();
+            DoctorListView.Items.Refresh();
+
+        }
+
+        private void SortDescRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            Update();
+            DoctorListView.Items.Refresh();
+        }
+
+        private void SortAscRadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+            Update();
+            DoctorListView.Items.Refresh();
+
+        }
+
+        private void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+            Update();
+            DoctorListView.Items.Refresh();
+
+        }
+
+        private void AddDoctor_Click(object sender, RoutedEventArgs e)
+        {
+            manager.MainFrame.Navigate(new AddEditPage(null));
+            Update();
+            DoctorListView.Items.Refresh();
+        }
+
+        private void editDoctor_Click(object sender, RoutedEventArgs e)
+        {
+            manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as DOCTORS));
+            Update();
+            DoctorListView.Items.Refresh();
+        }
+
+        private void deleteDoctor_Click(object sender, RoutedEventArgs e)
+        {
+            var currentDoctor = (sender as Button).DataContext as DOCTORS;
+            var currentDoctorHistory = PolyclinicEntities.GetContext().MEDICAL_HISTORY.ToList();
+            currentDoctorHistory = currentDoctorHistory.Where(p=>p.HISTORY_DOCTOR_ID ==currentDoctor.DOCTOR_ID).ToList();
+
+            if (currentDoctorHistory.Count!=0)
+            {
+                MessageBox.Show("Невозможно выполнить удаление, так как врач записан на прием!");
+               
+                
+
+            }
+            else
+            {
+                if (MessageBox.Show("Вы точно хотите удалить?", "Внимание!", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    PolyclinicEntities.GetContext().DOCTORS.Remove(currentDoctor);
+                    PolyclinicEntities.GetContext().SaveChanges();
+                    DoctorListView.ItemsSource = PolyclinicEntities.GetContext().DOCTORS.ToList();
+                    Update();
+                }
+
+            }
+
+
+
+
+        }
+
+        private void DoctorListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Update();
+            DoctorListView.Items.Refresh();
+        }
+
+        private void DoctorListView_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+
+            Update();
+
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+
+            if (Visibility == Visibility.Visible)
+            {
+                PolyclinicEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                DoctorListView.ItemsSource = PolyclinicEntities.GetContext().DOCTORS.ToList();
+                Update();
+            }
+
+        }
+    }
+}
